4a9bb2abfaff12b80cc6869e05cd139e55c22660
Jill <19cartji@wtahs.com>
2017-11-06 09:20:24 -0500

    Final code

---
+++ Address.Book/Contact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Address.Book
+{
+    class Contact
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public Contact(string name, string address)
+        {
+            Name = name;
+            Address = address;
+        }
+    }
+}
+
